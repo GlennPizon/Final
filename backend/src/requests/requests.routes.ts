e81b1d50a136f5e08e1929bfc23f5aded34d8cea
glenn.pizon2003@gmail.com <YY236@ccs.local>
2025-05-12 18:11:12 -0700

    request routes

--- backend/src/requests/requests.routes.ts
+++ backend/src/requests/requests.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { RequestController } from './request.controller';
 import { validate } from '../middleware/validate-request';
-import { createRequestSchema } from './request.schema';
+import { createRequestSchema, updateRequestSchema } from './request.schema';
 import { authorize } from '../middleware/authorize';
 import Role from '../utils/role';
 
@@ -12,7 +12,7 @@ router.get('/',authorize(Role.Admin), RequestController.getAll);
 router.get('/my', authorize(), RequestController.getMyRequests);
 router.get('/:id',authorize(), RequestController.getById);
 router.get('/employee/:employeeId',authorize(), RequestController.getByEmployeeId)
-router.put('/:id',authorize(Role.Admin), RequestController.update);
+router.put('/:id',authorize(Role.Admin), validate(updateRequestSchema), RequestController.update);
 router.delete('/:id',authorize(Role.Admin),RequestController.delete);
 
 export default router;
