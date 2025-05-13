e5f20c23653051c8908f420f7336e865237a78f8
glenn.pizon2003@gmail.com <YY236@ccs.local>
2025-05-12 18:09:51 -0700

    request routes config

--- backend/src/requests/requests.routes.ts
+++ backend/src/requests/requests.routes.ts
@@ -2,14 +2,17 @@ import { Router } from 'express';
 import { RequestController } from './request.controller';
 import { validate } from '../middleware/validate-request';
 import { createRequestSchema } from './request.schema';
+import { authorize } from '../middleware/authorize';
+import Role from '../utils/role';
 
 const router = Router();
 
-router.post('/', validate(createRequestSchema), RequestController.create);
-router.get('/', RequestController.getAll);
-router.get('/my', RequestController.getMyRequests);
-router.get('/:id', RequestController.getById);
-router.put('/:id', RequestController.update);
-router.delete('/:id', RequestController.delete);
+router.post('/',authorize(), validate(createRequestSchema), RequestController.create);
+router.get('/',authorize(Role.Admin), RequestController.getAll);
+router.get('/my', authorize(), RequestController.getMyRequests);
+router.get('/:id',authorize(), RequestController.getById);
+router.get('/employee/:employeeId',authorize(), RequestController.getByEmployeeId)
+router.put('/:id',authorize(Role.Admin), RequestController.update);
+router.delete('/:id',authorize(Role.Admin),RequestController.delete);
 
 export default router;
