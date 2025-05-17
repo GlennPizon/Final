107008c1c6cef7287dcd8bd6cd9d2bb7649450f1
unknown <YY236@ccs.local>
2025-05-16 23:51:20 -0700

    model config

--- frontend/PIZON/src/app/_models/alert.ts
+++ frontend/PIZON/src/app/_models/alert.ts
@@ -1,6 +1,15 @@
 export interface Alert {
-  id: string; // Unique ID for each alert
-  message: string; // The content of the alert
-  severity: 'info' | 'warning' | 'error' | 'success'; // Severity of the alert
-  timestamp: Date; // When the alert was triggered
+    id: string;
+    type: AlertType;
+    message: string;
+    autoClose: boolean;
+    keepAfterRouteChange: boolean;
+    fade: boolean;
+}
+
+export enum AlertType {
+    Success,
+    Error,
+    Info,
+    Warning
 }
--- frontend/PIZON/src/app/_models/role.ts
+++ frontend/PIZON/src/app/_models/role.ts
@@ -1,1 +1,17 @@
-export type Role = 'User' | 'Admin'
+export enum Role {
+    User = 'User',
+    Admin = 'Admin'
+}
+
+export enum Status {
+    Active = 'Active',
+    Inactive = 'Inactive'
+}
+
+export enum WorkflowStatus {
+    Pending = 'Pending',
+    Approved = 'Approved',
+    Rejected = 'Rejected'
+}
+
+
