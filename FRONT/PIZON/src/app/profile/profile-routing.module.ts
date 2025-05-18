d9a8db17f240d5c77e92501f286d37025462a1a7
Lenov PC <glenn.pizon2003@gmail.com>
2025-05-18 14:13:17 +0800

    profile

--- FRONT/PIZON/src/app/profile/profile-routing.module.ts
+++ FRONT/PIZON/src/app/profile/profile-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from '../profile/layout/layout.component';
+import { DetailsComponent } from './details/details.component';
+import { UpdateComponent } from './update/update.component';
+import { AuthGuard } from '../_helpers/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent, canActivate: [AuthGuard], children: [
+      { path: '', component: DetailsComponent },
+      { path: 'edit', component: UpdateComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ProfileRoutingModule { }
+export class ProfileRoutingModule {}
--- FRONT/PIZON/src/app/profile/profile.module.ts
+++ FRONT/PIZON/src/app/profile/profile.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { ProfileRoutingModule } from './profile-routing.module';
-
+import { DetailsComponent } from './details/details.component';
+import { UpdateComponent } from './update/update.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetailsComponent,
+    UpdateComponent,
+    LayoutComponent
+  ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ProfileRoutingModule
   ]
 })
-export class ProfileModule { }
+export class ProfileModule {}
