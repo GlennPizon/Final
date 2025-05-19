c7c3f8b45d0800822fac551840096765369a881d
Lenov PC <glenn.pizon2003@gmail.com>
2025-05-19 18:01:04 +0800

    config app

--- FRONT/PIZON/src/app/workflows/workflows.module.ts
+++ FRONT/PIZON/src/app/workflows/workflows.module.ts
@@ -1,26 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { WorkflowsRoutingModule } from './workflows-routing.module';
 import { ListComponent } from './list.component';
 import { WorkflowCreateComponent } from './create.component';
-import { AuthGuard } from '../_helpers/auth.guard';
-import { Role } from '../_models/role';
-
-const routes: Routes = [
-  {
-    path: ':employeeId',
-    component: ListComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: ':employeeId/create',
-    component: WorkflowCreateComponent,
-    canActivate: [AuthGuard],
-    data: { roles: [Role.Admin] }
-  }
-];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    WorkflowsRoutingModule
+  ],
+  declarations: [
+    ListComponent,
+    WorkflowCreateComponent
+  ]
 })
-export class WorkflowsRoutingModule { }
+export class WorkflowsModule { }
